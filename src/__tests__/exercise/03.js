21b373c6b4f5009475a8d929918582b1b4fec175
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-10 15:22:35 -0700

    exercise 3 use userEvent instaed of fireEvent

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,7 +2,9 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+// fires all events associated with an interaction (ex: mouseDown, mouseUp, click, etc)
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
@@ -15,8 +17,10 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  
+  userEvent.click(increment)  
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
