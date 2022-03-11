9d3ebcfc8cae64f6e30af4940fe07aafb1fd04e8
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-11 15:27:59 -0700

    use a setup function

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -6,25 +6,56 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
+// create setup function to avoid duplication
+function setup({initialProps} = {}) {
+  // get access to everything userCounter() returns
+  const result = {}
+  function TestComponent() {
+    result.current = useCounter(initialProps)
+    return null
+  }
+  
+  render(<TestComponent />)
+  return result
+}
+
 // testing without interacting with the DOM
 // helpful if you cant create a test component that uses it
 test('exposes the count and increment/decrement functions', () => {
-  // get access to everything userCounter() returns
-  let result
-  function TestComponent() {
-    result = useCounter()
-    return null
-  }
-
-  render(<TestComponent />)
-  // console.log(result)
-
-  expect(result.count).toBe(0)
+  const result = setup()
+  // need to use result.current because of binding and this result does not update when the component rerenders
+  expect(result.current.count).toBe(0)
 
   // act flushes the DOM after updates
-  act(() => result.increment())
-  expect(result.count).toBe(1)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
 
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 2}})
+
+  expect(result.current.count).toBe(2)
+
+  // act flushes the DOM after updates
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(2)
+})
+
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 2}})
+
+  expect(result.current.count).toBe(0)
+
+  // act flushes the DOM after updates
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
