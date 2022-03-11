7345b25505b53c9c175d3a250caa3f2173440600
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-11 14:44:31 -0700

    exercise 6 create a render helper method

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,17 +6,41 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+// can use this pattern for any providers
+// options alllows you to send somthing other than theme to forward onto the ThemeProvider
+function renderWithTheme(ui, {theme='light', ...options} = {}) {
+  const Wrapper = ({children}) => {
+    return (
+      <ThemeProvider initialTheme={theme}>
+        {children}
+      </ThemeProvider>
+    )
+  }
+  // render takes 2 args, the component and options
+  return render(ui, {wrapper: Wrapper, ...options})
+}
+
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+
+  renderWithTheme(<EasyButton>Easy</EasyButton>)
+  
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+  
 })
 
-/* eslint no-unused-vars:0 */
+test('renders with the dark styles for the dark theme', () => {
+  renderWithTheme(
+    <EasyButton>Easy</EasyButton>, 
+    {theme: 'dark'}
+  )
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+  
+})
